a57c3a438c56fb681ef19669015ff515b8cc7796
Anu Thomas <52321160+AnuAnnThomas@users.noreply.github.com>
2020-12-09 20:14:25 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,16 +124,6 @@
     offset: '80%'
   });
 
-  // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out-back",
-      once: true
-    });
-  }
-  $(window).on('load', function() {
-    aos_init();
-  });
+ 
 
 })(jQuery);
